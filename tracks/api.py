e1dc07f8347e576b494a48b78b5fd5d71c645a93
jhon <pioneer3692@gmail.com>
2022-02-05 19:02:49 +0900

    first commit

---
+++ tracks/api.py
@@ -0,0 +1,23 @@
+from typing import List, Optional
+from ninja import NinjaAPI
+from tracks.models import Track
+from tracks.schema import TrackSchema, NotFoundSchema
+
+
+api = NinjaAPI()
+
+
+@api.get("/tracks", response=List[TrackSchema])
+def tracks(request, title: Optional[str] = None):
+    if title:
+        return Track.objects.filter(title__icontains=title)
+    return Track.objects.all()
+
+
+@api.get("/tracks/{track_id}", response={200: TrackSchema, 404: NotFoundSchema})
+def track(request, track_id: int):
+    try:
+        track = Track.objects.get(pk=track_id)
+        return 200, track
+    except Track.DoesNotExist as e:
+        return 404, {"msg": "Track does not exist"}
