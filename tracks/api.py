a49c7c0e096a53b011892c33dc2aa0972966c3d2
jhon <pioneer3692@gmail.com>
2022-02-05 19:43:41 +0900

    added CRUD function

--- tracks/api.py
+++ tracks/api.py
@@ -21,3 +21,22 @@ def track(request, track_id: int):
         return 200, track
     except Track.DoesNotExist as e:
         return 404, {"msg": "Track does not exist"}
+
+
+@api.post("/tracks", response={201: TrackSchema})
+def create_track(request, track: TrackSchema):
+    print(f"sdsd{request}")
+    track = Track.objects.create(**track.dict())
+    return track
+
+
+@api.put("/tracks/{track_id}", response={200: TrackSchema, 404: NotFoundSchema})
+def change_track(request, track_id: int, data: TrackSchema):
+    try:
+        track = Track.objects.get(pk=track_id)
+        for attribute, value in data.dict().items():
+            setattr(track, attribute, value)
+        track.save()
+        return 200, track
+    except Track.DoesNotExist as e:
+        return 404, {"message": "Track does not exist"}
